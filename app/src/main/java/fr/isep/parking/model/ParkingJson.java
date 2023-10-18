6ac6cd89c23c35c8743a5fdeaff851271903ec60
Chiya <chia930429@gmail.com>
2023-10-18 13:12:21 +0200

    parking location appearance and marker change/ to do handler without internet connection

--- app/src/main/java/fr/isep/parking/model/ParkingJson.java
+++ app/src/main/java/fr/isep/parking/model/ParkingJson.java
@@ -1,55 +1,100 @@
 package fr.isep.parking.model;
 
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.util.List;
 public class ParkingJson {
-    // Define fields that match your JSON keys
+
     private String regpri;
     private String regpar;
-    private Geo geo;
-//    private int arrond;
     private String typsta;
-//    private int placal;
-//    private int plarel;
-//    private String zoneres;
-//    private String locsta;
-//    private String numvoie;
-//    private String typevoie;
-//    private String nomvoie;
-//    private String compnumvoie;
-//    private String locnum;
-//    private String parite;
-//    private double lon;
-//    private double lar;
-//    private double surface_calculee;
-//    private String signhor;
-//    private String signvert;
-//    private String confsign;
-//    private String typemob;
-//    private int nummob;
-//    private String plage_hor1_debut;
-//    private String plage_hor1_fin;
-//    private String plage_hor2_debut;
-//    private String plage_hor2_fin;
-//    private String plage_hor3_debut;
-//    private String plage_hor3_fin;
-//    private String id;
-//    private String id_old;
-//    private String c_voie_vp;
-//    private String n_sq_tv;
-//    private String numilot;
-//    private String numiris;
-//    private String zoneasp;
-//    private String stv;
-//    private String prefet;
-//    private String mtlast_edit_date_field;
-//    private String datereleve;
-//    private int n_voieadd;
-//    private int n_voieadf;
+    private int arrond;
+    private int placal;
+    private int plarel;
+    private String zoneres;
+    private String locsta;
+    private String numvoie;
+    private String typevoie;
+    private String nomvoie;
+    private String compnumvoie;
+    private String locnum;
+    private String parite;
+    private double lon;
+    private double lar;
+    //        private double surfaceCalculee;
+    private String signhor;
+    private String signvert;
+    private String confsign;
+    private String typemob;
+    private int nummob;
+    private String plageHor1Debut;
+    private String plageHor1Fin;
+    private String plageHor2Debut;
+    private String plageHor2Fin;
+    private String plageHor3Debut;
+    private String plageHor3Fin;
+    private String id;
+    private String idOld;
+    private String cVoieVp;
+    private String nSqTv;
+    private String numilot;
+    private String numiris;
+    private String zoneasp;
+    private String stv;
+    private String prefet;
+    private String mtlastEditDateField;
+    private String datereleve;
+    private int nVoieadd;
+    private int nVoieadf;
+    private double latitude;
+    private double longitude;
     private Geo_shape geo_shape;
-//    private Geo_point_2d geo_point_2d;
+    private Geo_point_2d geo_point_2d;
+
+    public static class Geo {
+        private double lon;
+        private double lat;
+    }
+
+    public static class Geo_shape {
+        private Geo geo;
+    }
+
+    public static class Geo_point_2d {
+        private double lon;
+        private double lat;
+    }
+
+
+    // Constructors
+//    public ParkingJson(String regpri, String regpar, String typsta, int arrond, int placal, int plarel, String zoneres, String locsta, int numvoie, String typevoie, String nomvoie, String compnumvoie, String locnum, String parite, double lon, double lar, double surfaceCalculee, String signhor, String signvert, String confsign, String typemob, int nummob, Geo geo, Geo_shape geo_shape) {
+//        this.regpri = regpri;
+//        this.regpar = regpar;
+//        this.typsta = typsta;
+//        this.arrond = arrond;
+//        this.placal = placal;
+//        this.plarel = plarel;
+//        this.zoneres = zoneres;
+//        this.locsta = locsta;
+//        this.numvoie = numvoie;
+//        this.typevoie = typevoie;
+//        this.nomvoie = nomvoie;
+//        this.compnumvoie = compnumvoie;
+//        this.locnum = locnum;
+//        this.parite = parite;
+//        this.lon = lon;
+//        this.lar = lar;
+//        this.surfaceCalculee = surfaceCalculee;
+//        this.signhor = signhor;
+//        this.signvert = signvert;
+//        this.confsign = confsign;
+//        this.typemob = typemob;
+//        this.nummob = nummob;
+//        this.geo = geo;
+//        this.geo_shape = geo_shape;
+//    }
+
+
+    // Getter and setter methods for each field
 
     public String getRegpri() {
         return regpri;
@@ -67,78 +112,331 @@ public class ParkingJson {
         this.regpar = regpar;
     }
 
-    public double getLat() {
-        return geo.lat;
+    public String getTypsta() {
+        return typsta;
     }
 
-    public void setLat(double lat) {
-        this.geo.lat = lat;
+    public void setTypsta(String typsta) {
+        this.typsta = typsta;
+    }
+
+    public int getArrond() {
+        return arrond;
+    }
+
+    public void setArrond(int arrond) {
+        this.arrond = arrond;
+    }
+
+    public int getPlacal() {
+        return placal;
+    }
+
+    public void setPlacal(int placal) {
+        this.placal = placal;
+    }
+
+    public int getPlarel() {
+        return plarel;
+    }
+
+    public void setPlarel(int plarel) {
+        this.plarel = plarel;
+    }
+
+    public String getZoneres() {
+        return zoneres;
+    }
+
+    public void setZoneres(String zoneres) {
+        this.zoneres = zoneres;
+    }
+
+    public String getLocsta() {
+        return locsta;
+    }
+
+    public void setLocsta(String locsta) {
+        this.locsta = locsta;
+    }
+
+    public String getNumvoie() {
+        return numvoie;
+    }
+
+    public void setNumvoie(String numvoie) {
+        this.numvoie = numvoie;
+    }
+
+    public String getTypevoie() {
+        return typevoie;
+    }
+
+    public void setTypevoie(String typevoie) {
+        this.typevoie = typevoie;
+    }
+
+    public String getNomvoie() {
+        return nomvoie;
+    }
+
+    public void setNomvoie(String nomvoie) {
+        this.nomvoie = nomvoie;
+    }
+
+    public String getCompnumvoie() {
+        return compnumvoie;
+    }
+
+    public void setCompnumvoie(String compnumvoie) {
+        this.compnumvoie = compnumvoie;
+    }
+
+    public String getLocnum() {
+        return locnum;
+    }
+
+    public void setLocnum(String locnum) {
+        this.locnum = locnum;
+    }
+
+    public String getParite() {
+        return parite;
+    }
+
+    public void setParite(String parite) {
+        this.parite = parite;
     }
 
     public double getLon() {
-        return geo.lon;
+        return lon;
     }
 
     public void setLon(double lon) {
-        this.geo.lon = lon;
+        this.lon = lon;
     }
 
-
-    public class Geo {
-        private double lon;
-        private double lat;
+    public double getLar() {
+        return lar;
     }
 
-    public class Geo_shape {
-        private Geo geo;
+    public void setLar(double lar) {
+        this.lar = lar;
     }
 
-    public class Geo_point_2d {
-        private double lon;
-        private double lat;
+//        public double getSurfaceCalculee() {
+//            return surfaceCalculee;
+//        }
+//
+//        public void setSurfaceCalculee(double surfaceCalculee) {
+//            this.surfaceCalculee = surfaceCalculee;
+//        }
+
+    public String getSignhor() {
+        return signhor;
     }
 
+    public void setSignhor(String signhor) {
+        this.signhor = signhor;
+    }
 
-    private ParkingJson parseParkingData(JSONObject json) {
-        // Implement parsing logic based on your API response structure
-        ParkingJson parkingData = new ParkingJson();
-        try {
-            parkingData.setRegpri(json.getString("regpri"));
-            parkingData.setRegpar(json.getString("regpar"));
+    public String getSignvert() {
+        return signvert;
+    }
 
-            parkingData.setLon(json.getDouble("lon"));
-            parkingData.setLat(json.getDouble("lar"));
-//            parkingData.setSurface_calculee(json.getDouble("surface_calculee"));
-//            parkingData.setSignhor(json.getString("signhor"));
-//            parkingData.setSignvert(json.getString("signvert"));
-//            parkingData.setConfsign(json.getString("confsign"));
-//            parkingData.setTypemob(json.getString("typemob"));
-//            parkingData.setNummob(json.getInt("nummob"));
-//            parkingData.setPlage_hor1_debut(json.getString("plage_hor1_debut"));
-//            parkingData.setPlage_hor1_fin(json.getString("plage_hor1_fin"));
-//            parkingData.setPlage_hor2_debut(json.getString("plage_hor2_debut"));
-//            parkingData.setPlage_hor2_fin(json.getString("plage_hor2_fin"));
-//            parkingData.setPlage_hor3_debut(json.getString("plage_hor3_debut"));
-//            parkingData.setPlage_hor3_fin(json.getString("plage_hor3_fin"));
-//            parkingData.setId(json.getString("id"));
-//            parkingData.setId_old(json.getString("id_old"));
-//            parkingData.setC_voie_vp(json.getString("c_voie_vp"));
-//            parkingData.setN_sq_tv(json.getString("n_sq_tv"));
-//            parkingData.setNumilot(json.getString("numilot"));
-//            parkingData.setNumiris(json.getString("numiris"));
-//            parkingData.setZoneasp(json.getString("zoneasp"));
-//            parkingData.setStv(json.getString("stv"));
-//            parkingData.setPrefet(json.getString("prefet"));
-//            parkingData.setMtlast_edit_date_field(json.getString("mtlast_edit_date_field"));
-//            parkingData.setDatereleve(json.getString("datereleve"));
-//            parkingData.setN_voieadd(json.getInt("n_voieadd"));
-//            parkingData.setN_voieadf(json.getInt("n_voieadf"));
-            // Add other fields here
-            // You might need to implement geo_shape and geo_point_2d parsing logic if needed
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return parkingData;
+    public void setSignvert(String signvert) {
+        this.signvert = signvert;
+    }
+
+    public String getConfsign() {
+        return confsign;
+    }
+
+    public void setConfsign(String confsign) {
+        this.confsign = confsign;
+    }
+
+    public String getTypemob() {
+        return typemob;
+    }
+
+    public void setTypemob(String typemob) {
+        this.typemob = typemob;
+    }
+
+    public int getNummob() {
+        return nummob;
+    }
+
+    public void setNummob(int nummob) {
+        this.nummob = nummob;
+    }
+
+    public String getPlageHor1Debut() {
+        return plageHor1Debut;
+    }
+
+    public void setPlageHor1Debut(String plageHor1Debut) {
+        this.plageHor1Debut = plageHor1Debut;
+    }
+
+    public String getPlageHor1Fin() {
+        return plageHor1Fin;
+    }
+
+    public void setPlageHor1Fin(String plageHor1Fin) {
+        this.plageHor1Fin = plageHor1Fin;
+    }
+
+    public String getPlageHor2Debut() {
+        return plageHor2Debut;
+    }
+
+    public void setPlageHor2Debut(String plageHor2Debut) {
+        this.plageHor2Debut = plageHor2Debut;
+    }
+
+    public String getPlageHor2Fin() {
+        return plageHor2Fin;
+    }
+
+    public void setPlageHor2Fin(String plageHor2Fin) {
+        this.plageHor2Fin = plageHor2Fin;
+    }
+
+    public String getPlageHor3Debut() {
+        return plageHor3Debut;
+    }
+
+    public void setPlageHor3Debut(String plageHor3Debut) {
+        this.plageHor3Debut = plageHor3Debut;
+    }
+
+    public String getPlageHor3Fin() {
+        return plageHor3Fin;
+    }
+
+    public void setPlageHor3Fin(String plageHor3Fin) {
+        this.plageHor3Fin = plageHor3Fin;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdOld() {
+        return idOld;
+    }
+
+    public void setIdOld(String idOld) {
+        this.idOld = idOld;
+    }
+
+    public String getCVoieVp() {
+        return cVoieVp;
+    }
+
+    public void setCVoieVp(String cVoieVp) {
+        this.cVoieVp = cVoieVp;
+    }
+
+    public String getNSqTv() {
+        return nSqTv;
+    }
+
+    public void setNSqTv(String nSqTv) {
+        this.nSqTv = nSqTv;
+    }
+
+    public String getNumilot() {
+        return numilot;
+    }
+
+    public void setNumilot(String numilot) {
+        this.numilot = numilot;
+    }
+
+    public String getNumiris() {
+        return numiris;
+    }
+
+    public void setNumiris(String numiris) {
+        this.numiris = numiris;
+    }
+
+    public String getZoneasp() {
+        return zoneasp;
+    }
+
+    public void setZoneasp(String zoneasp) {
+        this.zoneasp = zoneasp;
+    }
+
+    public String getStv() {
+        return stv;
+    }
+
+    public void setStv(String stv) {
+        this.stv = stv;
+    }
+
+    public String getPrefet() {
+        return prefet;
+    }
+
+    public void setPrefet(String prefet) {
+        this.prefet = prefet;
+    }
+
+    public String getMtlastEditDateField() {
+        return mtlastEditDateField;
+    }
+
+    public void setMtlastEditDateField(String mtlastEditDateField) {
+        this.mtlastEditDateField = mtlastEditDateField;
+    }
+
+    public String getDatereleve() {
+        return datereleve;
+    }
+
+    public void setDatereleve(String datereleve) {
+        this.datereleve = datereleve;
+    }
+
+    public int getNVoieadd() {
+        return nVoieadd;
+    }
+
+    public void setNVoieadd(int nVoieadd) {
+        this.nVoieadd = nVoieadd;
+    }
+
+    public int getNVoieadf() {
+        return nVoieadf;
+    }
+
+    public void setNVoieadf(int nVoieadf) {
+        this.nVoieadf = nVoieadf;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
--- app/src/main/java/fr/isep/parking/view/MainActivity.java
+++ app/src/main/java/fr/isep/parking/view/MainActivity.java
@@ -2,13 +2,16 @@ package fr.isep.parking.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -28,6 +31,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 import fr.isep.parking.R;
+import fr.isep.parking.model.ParkingJson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,13 +54,20 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onMapLoaded() {
                         LatLngBounds.Builder bounds = new LatLngBounds.Builder();
-                        for (LatLng latlng : model.latLngList) {
+                        for (ParkingJson parking : model.parkingList) {
+                            //todo when  does not catch the data
+                            LatLng latLng = new LatLng(parking.getLatitude(), parking.getLongitude());
 
                             MarkerOptions marker = new MarkerOptions()
-                                    .position(latlng)
-                                            .title("Mark");
+                                    .position(latLng)
+                                    .title("Address: " + parking.getNumvoie() + " " + parking.getTypevoie() + "," + parking.getNomvoie() + "," + parking.getArrond() + "\n" +
+                                            "Locate on: " + parking.getLocnum())
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
                             googleMap.addMarker(marker);
-                            bounds.include(latlng);
+                            bounds.include(latLng);
+
+
                         }
                         googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20));
                     }
@@ -73,10 +84,12 @@ public class MainActivity extends AppCompatActivity {
         String data = "";
         ArrayList<LatLng> latLngList = new ArrayList<LatLng>(); // Store LatLng objects
         ArrayList<String> result;
+        ArrayList<ParkingJson> parkingList = new ArrayList<ParkingJson>();
         @Override
         public void run() {
             try {
-                URL apiUrl = new URL("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/stationnement-voie-publique-emplacements/records?limit=10");
+                //to do when there is no internet connection use toast to show the situation
+                URL apiUrl = new URL("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/stationnement-voie-publique-emplacements/records?limit=100");
                 HttpURLConnection httpURLConnection = (HttpURLConnection) apiUrl.openConnection();
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader bf;
@@ -90,26 +103,52 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (!data.isEmpty()) {
                     JSONObject jsonObject = new JSONObject(data);
+                    Log.d("res::", data.toString());
                     JSONArray results = jsonObject.getJSONArray("results");
                     for (int i = 0; i < results.length(); i++) {
                         JSONObject item = results.getJSONObject(i);
-                        String regpri = item.getString("regpri");
+                        ParkingJson parking = new ParkingJson();
+                        parking.setRegpri(item.getString("regpri"));
+                        parking.setRegpar(item.getString("regpar"));
+                        parking.setTypsta(item.getString("typsta"));
+                        parking.setArrond(item.getInt("arrond"));
+                        parking.setPlacal(item.getInt("placal"));
+                        parking.setArrond(item.getInt("arrond"));
+                        parking.setPlacal(item.getInt("placal"));
+                        parking.setPlarel(item.getInt("plarel"));
+                        parking.setZoneres(item.getString("zoneres"));
+                        parking.setLocsta(item.getString("locsta"));
+                        parking.setNumvoie(item.getString("numvoie"));
+                        parking.setTypevoie(item.getString("typevoie"));
+                        parking.setNomvoie(item.getString("nomvoie"));
+                        parking.setCompnumvoie(item.getString("compnumvoie"));
+                        parking.setLocnum(item.getString("locnum"));
+
                         JSONObject geoShape = item.getJSONObject("geo_shape");
                         JSONObject geometry = geoShape.getJSONObject("geometry");
                         JSONArray coordinates = geometry.getJSONArray("coordinates");
-
                         double lat = coordinates.getDouble(1);
                         double lon = coordinates.getDouble(0);
-                        System.out.println(String.format("Lat: %f, Lon: %f", lat, lon));
+                        parking.setLatitude(lat);
+                        parking.setLongitude(lon);
+
+                        // Add the ParkingJson object to the list
+                        parkingList.add(parking);
+
                         LatLng latlng = new LatLng(lat, lon);
                         latLngList.add(latlng);
                     }
                 }
             } catch (MalformedURLException e) {
+                Log.d("error","MalformedURLException");
                 e.printStackTrace();
             } catch (JSONException e) {
+                Log.d("error","JSONException");
                 throw new RuntimeException(e);
             } catch (IOException e) {
+                Log.d("error","IOException");
+//                Toast.makeText(context, "Network error. Please check your internet connection.", Toast.LENGTH_LONG).show();
+
                 throw new RuntimeException(e);
             }
         }
